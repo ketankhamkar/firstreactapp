6f1cb6b5690b8b33b62f8fd4e82ba11c53b3bf76
ketan <ketankhamkar588@gmail.com>
2024-12-20 11:34:14 +0000

    change routes

--- src/RoutesApp/index.js
+++ src/RoutesApp/index.js
@@ -1,4 +1,4 @@
-import { Route, Routes, HashRouter, Link } from "react-router-dom";
+import { Route, Routes, BrowserRouter, Link } from "react-router-dom";
 import React, { lazy, Suspense } from "react";
 const Services = lazy(() => import("../screens/Services"));
 const Contacts = lazy(() => import("../screens/Contacts"));
@@ -8,7 +8,7 @@ const Login = lazy(() => import("../screens/Login"));
 
 const RoutesApp = () => {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Suspense
         fallback={
           <div
@@ -31,7 +31,7 @@ const RoutesApp = () => {
           <Route path="/Login" Component={Login} />
         </Routes>
       </Suspense>
-    </HashRouter>
+    </BrowserRouter>
   );
 };
 export default RoutesApp;
